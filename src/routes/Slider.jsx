14a1e68e0a98331b52f6d3454df8f2c297d6920a
Octavio Martinez <omartinez1618@gmail.com>
2022-10-24 14:33:18 -0400

    agrgado slider

--- src/routes/Slider.jsx
+++ src/routes/Slider.jsx
@@ -1,9 +1,31 @@
-import { useState } from 'react'
-import { LinearProgress, Box } from "@mui/material"
+import { useState, useEffect } from 'react'
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@mui/material'
+import { query } from './../utils'
 
 
 export default function Load(){
-    const [progress, setProgress] = useState(0)
+    const [progress, setProgress] = useState(0);
+    const [items, setItems] = useState([]);
+
+    useEffect( () => {
+        // query.getData('https://loto.kavavdigital.com/wp-json/wp/v2/media');
+
+        fetch('https://loto.kavavdigital.com/wp-json/wp/v2/media')
+            .then( response => response.json() )
+            .then( json => {
+                console.log(json);
+                // saveData(json);
+                setItems(json);
+            } )
+    }, [] );
+
+    const saveData = datas => {
+        const imgs = [];
+        datas.forEach( data => {
+            query.getImageData( data.source_url );
+        });
+    }
     
     const styleContainer = {
         position: 'relative',
@@ -12,10 +34,29 @@ export default function Load(){
         left: '10%',
     }
 
-    
-    
-    return(
-        <Box style={styleContainer}>
-            <LinearProgress variant='determinate' value={progress}/>
-        </Box>)
+    return (
+        <Carousel
+            // index={index}
+            // onChange={handleChange}
+            interval={5000}
+            animation="slide"
+            // indicators={false}
+            // stopAutoPlayOnHover
+            // swipe
+            className="my-carousel"
+        >
+            {
+                items.map( (item, i) => <Item key={item.id} item={item} /> )
+            }
+        </Carousel>
+    )
+}
+
+function Item(props)
+{
+    return (
+        <Paper>
+            <img height='320' alt="" src={props.item.source_url} />
+        </Paper>
+    )
 }
